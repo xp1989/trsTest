327ba36b51c3ce4e6a99b8599c62afc38c83bd10
gitUserName <1353796554@qq.com>
2017-01-30 16:10:23 +0800

    在第一次的基础上修改后提交

--- gitTest/src/test/Helloworld.java
+++ gitTest/src/test/Helloworld.java
@@ -3,6 +3,8 @@ package test;
 public class Helloworld {
 		public static void main(String[] args) {
 			System.out.println("hhhhhh");
+			System.out.println("hhhhhh");
+			System.out.println("hhhhhh");
 		}
 
 }
