893bc069f694544bd34783fcff5c7f2e312e9016
gitUserName <1353796554@qq.com>
2017-02-06 16:13:09 +0800

    123

--- gitTest/src/test/Helloworld.java
+++ gitTest/src/test/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 		public static void main(String[] args) {
 			System.out.println("3435534");
 			System.out.println("22");
-			System.out.println("3332523523555325325");
+			System.out.println("000");
 		}
 
 }
