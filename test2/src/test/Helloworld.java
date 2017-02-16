b45437681bf3c79b84c553834722ce758da05798
xiepeng <1353796554@qq.com>
2017-02-16 13:33:06 +0800

    2017--2-16

--- test2/src/test/Helloworld.java
+++ test2/src/test/Helloworld.java
@@ -1,6 +1,8 @@
 package test;
 
 public class Helloworld {
+private 	int age ;
+	private String name;
 		public static void main(String[] args) {
 			System.out.println("3435534");
 
@@ -13,7 +15,9 @@ public class Helloworld {
 		
 
 			System.out.println("第一个在第二个添加后重新修改后提交");
-	
+			Helloworld hl = new Helloworld();
+		 
+			System.out.println(hl.age);
 			}
 
 }
