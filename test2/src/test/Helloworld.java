4267327cfd878f02bae46b52cd5291d2e4ba81ea
xiepeng <1353796554@qq.com>
2017-03-05 21:40:24 +0800

    000000

--- test2/src/test/Helloworld.java
+++ test2/src/test/Helloworld.java
@@ -1,11 +1,36 @@
 package test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Helloworld {
 private 	int age ;
 	private String name;
 		public static void main(String[] args) {
-			System.out.println("3435534");
-
+		 
+			String str[] = {"1 1","1  2","1   3"};
+			System.out.println("////////////////////////////////");
+			
+			List<String> asList = Arrays.asList(str);
+			System.out.println(asList);
+			String string = asList.toString();
+			System.out.println(string);
+			String replace = string.replace("[", "");
+			System.out.println(replace);
+			String replace2 = replace.replace("]", "");
+			System.out.println(replace2);
+			
+			String replace3 = replace.replaceAll(" ", "");
+			System.out.println(replace3);
+			
+			
+			System.out.println("////////////////////////////////");
+			
+			
+			
+			
+			
+			
 			System.out.println("22222222222222222222222222");
 			System.out.println("333发生的范德萨");
 			System.out.println("第二个eclipse项目添加文件");
